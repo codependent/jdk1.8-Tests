33808fa75b845770931e2d4a08639921c0fecd68
codependent <joseantonio.inigo@gmail.com>
2015-04-07 17:48:06 +0200

    minor change

--- src/main/java/com/josesa/jdk18/Application.java
+++ src/main/java/com/josesa/jdk18/Application.java
@@ -1,14 +1,13 @@
 package com.josesa.jdk18;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+
+@SpringBootApplication
+/*@EnableAutoConfiguration
 @Configuration
 @ComponentScan(basePackages={"com.josesa.jdk18.service" , "com.josesa.jdk18.dao"})
-@EntityScan(basePackages="com.josesa.jdk18.entity")
+@EntityScan(basePackages="com.josesa.jdk18.entity")*/
 public class Application {
 
 }
